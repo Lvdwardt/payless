b25fb887b8d46655aac8567ede32aadbe3422dc6
Lvdwardt <leon.vandewardt@deptagency.com>
2024-11-01 12:02:10 +0100

    feat: add support for new google discover feed search.app links

--- src/hooks/useLinkToArchive.tsx
+++ src/hooks/useLinkToArchive.tsx
@@ -1,9 +1,10 @@
-const BASE_URL = "https://archive.is/";
 import { useEffect, useState } from "react";
 import { getArchive } from "@/utils/getArchive";
 import useLocalStorageState from "use-local-storage-state";
 import getArticle from "@/utils/getArticle";
 import { Font } from "@/types";
+
+const BASE_URL = "https://archive.is/";
 
 export default function useLinkToArchive(font: Font) {
   const [query, setQuery] = useState("");
@@ -24,24 +25,16 @@ export default function useLinkToArchive(font: Font) {
 
   useEffect(() => {
     async function fetchData() {
-      const params = new URLSearchParams(window.location.search);
-      let query = params.get("text") || "";
-      if (query.includes("http")) {
-        query = query.slice(query.indexOf("http"));
-      }
-      if (query.includes("?")) {
-        query = query.slice(0, query.indexOf("?"));
-      }
+      const query = getQuery();
 
-      if (query === "") {
-        query = window.location.pathname.slice(1);
-      }
-
-      if (query === "") {
+      if (!query) {
         return;
       }
+
       setQuery(query);
+      console.log(query);
       const link = await getArchive(query, BASE_URL);
+
       if (link && link !== "No link found" && link !== "Not working") {
         setTimeout(() => {
           setArticleLink(link);
@@ -54,16 +47,57 @@ export default function useLinkToArchive(font: Font) {
           window.location.replace(`${BASE_URL}?run=1&url=${query}`);
         }, timeBeforeRedirect);
       } else if (link === "Not working") {
-        // website user wants to archive is not working
-        setError(
-          `I'm sorry, ${query.split("/")[2]} is not working via this method.`
-        );
+        const domain = new URL(query).hostname;
+        setError(`I'm sorry, ${domain} is not working via this method.`);
       }
     }
-    fetchData();
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchData();
   }, []);
+
+  function getQuery() {
+    let query = getQueryParam("text");
+
+    if (!query) {
+      query = extractHrefFromWindow();
+    }
+    query = processQuery(query);
+
+    return query;
+  }
+
+  function getQueryParam(paramName: string) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(paramName) || "";
+  }
+
+  function extractHrefFromWindow() {
+    const href: string = decodeURIComponent(window.location.href);
+    return href.includes("search.app")
+      ? extractLinkFromSearchApp(href)
+      : window.location.pathname.slice(1);
+  }
+
+  function extractLinkFromSearchApp(str: string) {
+    const linkMatch = str.match(/link=([^&]+)/);
+    return linkMatch ? linkMatch[1] : str;
+  }
+
+  function processQuery(query: string) {
+    if (query.includes("http")) {
+      query = query.substring(query.indexOf("http"));
+    }
+
+    if (query.includes("search.app")) {
+      query = extractLinkFromSearchApp(query);
+    }
+
+    if (query.includes("?")) {
+      query = query.substring(0, query.indexOf("?"));
+    }
+
+    return query;
+  }
 
   return { isInstalled, hasQuery, article, articleLink, error };
 }
