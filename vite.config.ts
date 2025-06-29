3b6c2e296d4351b4e62a4b6e9adc70d033cea1be
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2025-06-29 16:37:14 +0200

    fix: update path resolution in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [
@@ -76,7 +80,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": "./src",
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
